d59037df7f1cc7a51313e93da8e8890cab0714f1
LibardoLozano <lilozanoga@unal.edu.co>
2022-03-19 23:00:54 -0500

    Changes

--- src/SentenciaFor.java
+++ src/SentenciaFor.java
@@ -1,20 +1,31 @@
 public class SentenciaFor {
     public static void main(String[] args) {
         for (int i =0; i<=15 ; i++){   // declaramos la variable entera i y la validamos segun la condicion expuesta, si es verdadera se mantiene el ciclo del bucle for y a la vez por cada bucle la vamos incrementando 
-            System.out.println("i = " + i);  // imprimimosen pantalla cada valor por bucle
+            System.out.println("i = " + i);  // imprimimos en pantalla cada valor por bucle
         }
         for (int i =14; i>=0 ; i--)   System.out.println("i = " + i);   // tambien podemos ejecutar el for asi cuando solo hay una instruccion en el bucle
 
+        for (int i =0, j = 10; i < j; i++, j--) System.out.println(i + "----------" + j);  // podemos combinar condicionales y variables para definir el bucle
 
 
-        int i = 0;    // podemos inicializar la variable fuera del for, pero esta ya estaria al alcance del main
+        int m = 0;    // podemos inicializar la variable fuera del for, pero esta ya estaria al alcance del main
         for (;;){    // podemos dejarlo asi y generar la condicion mas adelante mediante un if con un break para salir
-            if (i<=5) {
+            if (m<=5) {
 
-                break;
+                System.out.println("m = " + m);
+                m++;
+                continue;
             }
-            System.out.println("i = " + i);
-            i++; // podemos dejar el incremento al final
+
+            break;// podemos dejar el incremento al final
+        }
+        for (int i = 0; i < 30; i++) {
+            if (i%2 ==0){
+
+
+
+            }
+
         }
 
     }
