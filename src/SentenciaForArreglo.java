d49142ae60d051218f4409ddd095cff09b2f1977
LibardoLozano <lilozanoga@unal.edu.co>
2022-03-20 10:22:14 -0500

    Changes

--- src/SentenciaForArreglo.java
+++ src/SentenciaForArreglo.java
@@ -1,6 +1,8 @@
+import javax.swing.*;
+
 public class SentenciaForArreglo {
     public static void main(String[] args) {
-        String [] nombre={"Andres","predro","pepe","camilo","carlos","mauricio","miguel"};
+        String [] nombre={"Andres","pedro","pepe","camilo","carlos","mauricio","miguel"};
 
         int count = nombre.length;
         for (int i = 0; i <count; i++) {
@@ -13,6 +15,18 @@ public class SentenciaForArreglo {
                 continue;// saltamos la iteracion si encuentra estos nombres
             }
             System.out.println("nombre = "+i+".- " + nombre[i]);
+        }
+
+        String buscar= JOptionPane.showInputDialog(null,"Ingrese un nombre");
+        boolean encontrado =false;
+        for (int i = 0; i <count; i++) {  // en este for llamamos los elementos del arreglo nombre pero vamos a escojer cuales no iterar
+            if (nombre[i].equalsIgnoreCase(buscar)){
+                encontrado =true;
+                break;
+            }else encontrado = false;
+
+            if (encontrado) System.out.println("Encontramos el nombre: "+ buscar);
+            else JOptionPane.showMessageDialog(null,buscar+" no fue encontrado en el sistema");
         }
 
     }
